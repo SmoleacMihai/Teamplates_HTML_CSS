e7ee9cab3768d1a4880cfca45674c8caa5e34581
SmoleacMihai <mihaismoleac2002@gmail.com>
2022-06-30 15:30:24 +0300

    first half of login page

--- responsiveWebsite/assets/js/app.js
+++ responsiveWebsite/assets/js/app.js
@@ -1,25 +1,6 @@
 
-const getImgSrc = (imgStr) => {
-    const div = document.createElement('div')
-    div.innerHTML = imgStr
-    const img = div.querySelector('img')
-    return img.src
-}
+
 const changeState = () =>{
-    let curentState = 1;
-    const states = ["assets\\img\\uncheckedCheckBox.png", "assets\\img\\checkedCheckBox.png"];
-    if (curentState == 1) {
-        document.getElementById('checkbox').src = states[curentState];
-        curentState = 0;
-    }
-    if(curentState == 0){
-        document.getElementById('checkbox').src = states[curentState];
-        curentState = 1;
-        console.log("Zashol");
-        
-    }
-    console.log(curentState)
-    // console.log(curentElementSrc)
-    // console.log(typeof(curentElementSrc))
+    
 }
 // changeState();
