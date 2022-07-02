d48c544811660100e80ad88bb538e2d17ce0beb3
SmoleacMihai <mihaismoleac2002@gmail.com>
2022-07-02 15:28:26 +0300

    started home page

--- responsiveWebsite/assets/js/app.js
+++ responsiveWebsite/assets/js/app.js
@@ -1,6 +1,11 @@
 
 
-const changeState = () =>{
-    
+const changeImg = () =>{
+    if(document.getElementById("checkBox").src == "assets/img/uncheckedCheckBox.png"){
+        document.getElementById("checkBox").src = "assets/img/checkedCheckBox.png";
+    }
+    if(document.getElementById("checkBox").src == "assets/img/checkedCheckBox.png"){
+        document.getElementById("checkBox").src = "assets/img/uncheckedCheckBox.png"
+    }    
 }
 // changeState();
